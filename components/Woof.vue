519afda8f14b9973aa79dfb42dfb19edef1a9690
Mark Mahoney <mrkmhny@users.noreply.github.com>
2018-03-03 00:51:10 -0500

    Add main image, title, and background to homepage

---
+++ components/Woof.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <img src="~/static/profile.png" id='main-image' alt="Woof">
+    <h1 id='site__title'>Mark Mahoney</h1>
+  </div>  
+</template>
+
+<style>
+  #main-image {
+    margin: 1.2em;
+    position: relative;
+    width: 100px;
+    height: 100px;
+    border: 3px solid #fff;
+    border-radius:100%;
+    animation: backgroundChange 50s infinite;
+  }
+
+  @keyframes backgroundChange {
+    0%   {background: rgba(102,152,204,.7);}
+    20%  {background: rgba(102,204,152,.7);}
+    40%  {background: rgba(152,204,102,.7);}
+    60%  {background: rgba(204,152,102,.7);}
+    80%  {background: rgba(152,102,204,.7);}
+    100% {background: rgba(102,152,204,.7);}
+  }
+
+  #site__title {
+    color:white;
+    font-family:"Anton";
+    text-transform: uppercase;
+    animation: shadowChange 50s infinite;
+  }
+
+  @keyframes shadowChange {
+    0%   {text-shadow:2px 3px 0px rgba(102,152,204,.7);}
+    20%  {text-shadow:2px 3px 0px rgba(102,204,152,.7);}
+    40%  {text-shadow:2px 3px 0px rgba(152,204,102,.7);}
+    60%  {text-shadow:2px 3px 0px rgba(204,152,102,.7);}
+    80%  {text-shadow:2px 3px 0px rgba(152,102,204,.7);}
+    100% {text-shadow:2px 3px 0px rgba(102,152,204,.7);}
+  }
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Hello world!</h1>
+  <div id='app'>
+    <woof/>
+  </div>
 </template>
+
+<script>
+import Woof from '~/components/Woof'
+export default {
+  name: 'Home',
+  components: { Woof }
+}
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Anton');
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+  #app {
+    background-image: url('~/static/bg.jpg');
+  }
+</style>
